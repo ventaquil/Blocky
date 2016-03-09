1039b583d9137860bdb75804ea97fc3be2dde352
ventaquil <konrad.golawski@gmail.com>
2016-03-09 01:54:28 +0100

    Update make.bash console script

--- make.bash
+++ make.bash
@@ -1,4 +1,16 @@
 #!/bin/bash
+if [ $# -gt 0 ]
+then
+    path=$1
+else
+    path="."
+fi
+
+if [ "${path:${#path}-1:1}" == "/" ]
+then
+    path=${path:0:${#path}-1}
+fi
+
 javac Blocky.java
-jar cfe Blocky.jar Blocky *
+jar cfe "$path/Blocky.jar" Blocky *
 
