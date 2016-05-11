61aebd2a3532380b19cf505c484d65f651beee6f
Konrad Goławski <konrad.golawski@gmail.com>
2016-05-11 16:43:21 +0200

    Bash command file update

--- make.bash
+++ make.bash
@@ -1,21 +1,43 @@
 #!/bin/bash
+
+verbose=false
+execute=false
+
+function write {
+    if $verbose
+    then
+        echo $1
+    fi
+}
+
+path="."
+
+if [ $# -gt 0 ]
+then
+    for (( i=1; $i<=$#; i=$i+1 ))
+    do
+        if [ "${!i}" == "-v" ]
+        then
+            verbose=true
+        elif [ "${!i}" == "-e" ]
+        then
+            execute=true
+        else
+            path=${!i}
+        fi
+    done
+fi
+
 if [ `ls -1 | grep class | wc -l` -gt 0 ]
 then
-    echo "> rm *.class"
+    write "> rm *.class"
     rm *.class
 fi
 
 if [ `ls -1 | grep jar | wc -l` -gt 0 ]
 then
-    echo "> rm *.jar"
+    write "> rm *.jar"
     rm *.jar
-fi
-
-if [ $# -gt 0 ]
-then
-    path=$1
-else
-    path="."
 fi
 
 if [ "${path:${#path}-1:1}" == "/" ]
@@ -23,20 +45,26 @@ then
     path=${path:0:${#path}-1}
 fi
 
-echo "> javac Blocky.java"
+write "> javac Blocky.java"
 javac Blocky.java
 
-echo "> jar cfe Blocky.jar Blocky *.class resources/*"
+write "> jar cfe Blocky.jar Blocky *.class resources/*"
 jar cfe Blocky.jar Blocky *.class resources/*
 
-echo "> chmod +x Blocky.jar"
+write "> chmod +x Blocky.jar"
 chmod +x Blocky.jar
 
 if [ "$path" != "." ]
 then
-    echo "> mv Blocky.jar \"$path/Blocky.jar\""
+    write "> mv Blocky.jar \"$path/Blocky.jar\""
     mv Blocky.jar "$path/Blocky.jar"
 fi
 
-echo "> rm *.class"
+write "> rm *.class"
 rm *.class
+
+if $execute
+then
+    write "> java -jar \"$path/Blocky.jar\""
+    java -jar "$path/Blocky.jar"
+fi
